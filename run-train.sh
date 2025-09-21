0e8453a1760c1f8971a02a6c2a073c5fbb0cac9e
rick chen <admin@rickruguichen.altostrat.com>
2025-09-21 16:24:17 +0000

    add deepep flag

--- run-train.sh
+++ run-train.sh
@@ -42,7 +42,7 @@ NVSHMEM_HCA_LIST=f'mlx5_{NODE_RANK}:1' NVSHMEM_ENABLE_NIC_PE_MAPPING=1  PYTORCH_
         --distributed-timeout-minutes 60 \
         --tensor-model-parallel-size 2 \
         --pipeline-model-parallel-size 8 \
-        --expert-model-parallel-size 1 \
+        --expert-model-parallel-size 2 \
         --context-parallel-size 1 \
         --expert-tensor-parallel-size 4 \
         --use-distributed-optimizer  \
@@ -103,7 +103,6 @@ NVSHMEM_HCA_LIST=f'mlx5_{NODE_RANK}:1' NVSHMEM_ENABLE_NIC_PE_MAPPING=1  PYTORCH_
         --moe-router-topk 8 \
         --moe-token-dispatcher-type flex \
         --moe-enable-deepep \
-        --moe-token-capacity-factor 1.0 \
         --moe-router-pre-softmax  \
         --moe-aux-loss-coeff 1e-4 \
         --moe-router-group-topk 1 \
